3c8ed7c8a95717067cf23f656e64ba0b10ed145b
yoshuan <yaon12100@gmail.com>
2018-07-28 15:56:21 +0800

    add page

---
+++ js/parallax scrolling.js
@@ -0,0 +1,7 @@
+//var img = window.offsetTop;
+//var image = document.getElementsByClassName('graphic_001');
+//var speed = 0.8;
+//function parallex () {
+//	image.style.top = window.offsetTop * speed +'px';
+//}
+//window.addEventListener('scroll',parallex)
